42ef2919425264200cdc733d6411ec6116469a7e
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2024-08-03 10:43:16 +0300

    chore: add sort method

--- src/methods/arrayMethodJoin.js
+++ src/methods/arrayMethodJoin.js
@@ -1,11 +1,24 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function (separator = ',') {
+    let joinedString = '';
+    const lastIndex = this.length - 1;
+    const ignoredValues = [null, undefined];
+
+    for (let i = 0; i <= lastIndex; i++) {
+      const character = this[i];
+
+      if (!ignoredValues.includes(character)) {
+        joinedString += character;
+      }
+
+      if (i !== lastIndex) {
+        joinedString += separator;
+      }
+    }
+
+    return joinedString;
   };
 }
 
